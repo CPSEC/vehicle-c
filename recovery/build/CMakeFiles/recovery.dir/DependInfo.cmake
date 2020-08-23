e8953177705be39dd3167c12798dd2f7f24882e8
William Zhang <william@Williams-MacBook-Pro.local>
2020-08-23 16:51:09 -0400

    Updated build file

---
+++ recovery/build/CMakeFiles/recovery.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/vc/vehicle-c/recovery/lib/bcm2835.c" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/lib/bcm2835.c.o"
+  "/home/pi/vc/vehicle-c/recovery/lib/pca9685.c" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/lib/pca9685.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".././src"
+  ".././lib"
+  ".././lib/raspicam"
+  "/usr/local/include"
+  "/usr/local/include/opencv4"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/vc/vehicle-c/recovery/lib/MiniPID.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/lib/MiniPID.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/lib/as5048a.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/lib/as5048a.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/src/camera.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/src/camera.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/src/car.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/src/car.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/src/image.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/src/image.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/src/main.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/src/main.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/src/part.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/src/part.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/src/pca.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/src/pca.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/src/remote.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/src/remote.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/src/servoPID.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/src/servoPID.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/src/share.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/src/share.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/src/speed.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/src/speed.cpp.o"
+  "/home/pi/vc/vehicle-c/recovery/src/throttlePID.cpp" "/home/pi/vc/vehicle-c/recovery/build/CMakeFiles/recovery.dir/src/throttlePID.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././src"
+  ".././lib"
+  ".././lib/raspicam"
+  "/usr/local/include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
